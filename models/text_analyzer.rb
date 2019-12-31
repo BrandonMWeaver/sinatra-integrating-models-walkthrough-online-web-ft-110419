5627d96c2bc9868cbfc99e60a68c25355f79ef4b
Brandon Weaver <a7xncob@gmail.com>
2019-12-31 03:36:05 +0000

    Done.

--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,44 @@
-# Your TextAnalyzer model code will go here.
+class TextAnalyzer
+  
+  attr_reader :text
+  
+  def initialize(text)
+    @text = text.downcase
+  end
+  
+  def count_of_words
+    words = @text.split(' ')
+    return words.size
+  end
+  
+  def count_of_vowels
+    return @text.scan(/[aeiou]/).size
+  end
+  
+  def count_of_constants
+    return @text.scan(/[bcdfghjklmnpqrstvwxyz]/).size
+  end
+  
+  def most_used_letter
+    s1 = @text.gsub(/[^a-z]/, '')
+    arr = s1.split('')
+    arr1 = arr.uniq
+    arr2 = {}
+    
+    arr1.map do |c|
+      arr2[c] = arr.count(c)
+    end
+    
+    biggest = { arr2.keys.first => arr2.values.first }
+    
+    arr2.each do |key, value|
+      if value > biggest.values.first
+        biggest = {}
+        biggest[key] = value
+      end
+    end
+    
+    return biggest
+  end
+  
+end
